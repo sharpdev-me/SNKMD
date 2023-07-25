2d19417f8b937f46602292131d0e89dc533f8263
SharpDev <digidev.devx@gmail.com>
2023-07-25 10:11:04 -0700

    finally done with the action

--- build/build_windows.sh
+++ build/build_windows.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-echo $PWD
 cd build
-echo $PWD
 
 7z a -r game.zip ../ -xr!engine/love -xr!build -xr!.git -xr!*.moon -xr!conf.lua -xr!.gitignore -xr!.vscode
 mv game.zip game.love
 
 mkdir -p windows/build64
 
-cat windows/love64/love.exe game.love > windows/build64/SNKRX.exe
+cat ../engine/love.exe game.love > windows/build64/SNKRX.exe
 
 cp ../engine/love/*.dll windows/build64/
 cp ../engine/love/*.txt windows/build64/
