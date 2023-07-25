eb469800b35d83c148dcc3f2dc9ae85613823914
SharpDev <digidev.devx@gmail.com>
2023-07-25 09:35:59 -0700

    add build system

---
+++ build/build_windows.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+7z a -r game.zip ../ -xr!engine/love -xr!build -xr!.git -xr!*.moon -xr!conf.lua -xr!.gitignore -xr!.vscode
+mv game.zip game.love
+
+mkdir -p windows/build64
+
+cat windows/love64/love.exe game.love > windows/build64/SNKRX.exe
+
+cp ../engine/love/*.dll windows/build64/
+cp ../engine/love/*.txt windows/build64/
+
+7z a windows/SNKRX-windows64.zip windows/build64/
+
+
+## Done!
+rm game.love
